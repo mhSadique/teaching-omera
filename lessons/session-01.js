f19e2441bf05c70e3d7bffa0294f63d1960bc4d1
Sadique <mhsadique1307@gmail.com>
2022-10-08 22:28:31 +0600

    started

---
+++ lessons/session-01.js
@@ -0,0 +1,53 @@
+// ********* Background **********//
+// Little history of JavaScript
+// How computer understands JavaScript and other programming languages in general
+// How to run JavaScript code in our machines
+
+
+// ********* Data Types - 8 of them **********//
+
+// 1. string 
+// 2. number 
+
+// boolean, null, undefined, object, symbol, BigInt
+
+// ********* Variable **********//
+
+// creating a variable and assigning a value to it in the same line
+let myName = 'omera' // assigning a string to a variable
+myName = 'akram' // changing the value
+
+
+let numberOfStudent // creating a variable without a value
+numberOfStudent = 50 // asign a value later in a different line
+
+const myBirthday = '28 February' // assigning a number to a constant variable, can not be changed later
+
+var myPetName = 'cutie' // same as 'let' but ancient, has some special behavior, found in old codebases 
+
+// variable naming rules
+// 1. can not start with a number
+// 2. can not contain any special character other than _ and $
+// 3. should be meaningful
+// 4. should be camel-cased
+
+
+let $_myFavouriteFlowerName = 'tulip' // correct naming
+const _ = 'something' // correct naming
+
+// let wrong-VariableName = 'wrong naming'
+// This variable name will raise an error, 
+// because it is using a - (hyphen) which is a special character other than a _ or a $
+
+// ** copy a value of a variable to another variable
+
+// we can see output using console.log()
+// which is a built-in function from JS
+console.log(_)
+
+
+
+console.log(myName)
+console.log(myAge)
+
+
---
+++ lessons/session-02.js
@@ -0,0 +1,22 @@
+// string, 
+// number, 
+
+// boolean, null, undefined, object, symbol, BigInt
+
+
+let myName = 'omera'
+myName = 'akaram'
+
+const myAge = 20
+
+let $_myMotherName = 'xyz'
+const _ = 'something'
+
+console.log(_)
+
+
+
+console.log(myName)
+console.log(myAge)
+
+
