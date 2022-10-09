0db5fd21af0799237a99c7c576b409275b4de622
Sadique <mhsadique1307@gmail.com>
2022-10-09 08:36:49 +0600

    2nd session done

--- lessons/session-01.js
+++ lessons/session-01.js
@@ -4,50 +4,78 @@
 // How to run JavaScript code in our machines
 
 
-// ********* Data Types - 8 of them **********//
 
-// 1. string 
-// 2. number 
+// ********* Data Types - 8 (string, number, boolean, null, undefined, object, symbol, BigInt) **********//
 
-// boolean, null, undefined, object, symbol, BigInt
+
+
+// ********* statement **********//
+// A statement is a complete command to the machine
+
+
+
+// ********* string **********//
+let string1 = 'I am a string with "single" quotation marks';
+let string2 = "I am a string with 'double' quotation marks";
+let string3 = `I am a string with backticks and I have a special behavior`; // called string literal or template literal 
+
+
+// special behavior of string literal
+const stringLiteralSpecialBehavior = 'if I found a valid expression inside ${}, first I evaluate it and, then make it a part of me';
+const stringLiteral = `My special behavior is ${stringLiteralSpecialBehavior}`;
+
+
+
+// ********* number **********//
+let integer = 44;
+let decimalPointNumber = 34.7; // also called floating point number
+const PI = 3.1416; // another decimal point number
+
+
+
 
 // ********* Variable **********//
 
 // creating a variable and assigning a value to it in the same line
-let myName = 'omera' // assigning a string to a variable
+let myName = 'omera'; // assigning a string to a variable
 myName = 'akram' // changing the value later
 
 
-let numberOfStudent // creating a variable without a value
-numberOfStudent = 50 // asign a value later in a different line
+let numberOfStudentInClassOne; // creating a variable without a value
+numberOfStudentInClassOne = 50; // asign a value later in a different line
 
-const myBirthday = '28 February' // assigning a number to a constant variable, can not be changed later
 
-var myPetName = 'cutie' // same as 'let' but ancient, has some special behavior, found in old codebases 
+const myBirthday = '28 February'; // assigning a number to a constant variable, can not be changed later
+var myPetName = 'cutie'; // same as 'let' but ancient, has some special behavior, found in old codebases 
 
 // variable naming rules
 // 1. can not start with a number
 // 2. can not contain any special character other than _ and $
-// 3. should be meaningful
-// 4. should be camel-cased
+// 3. should not contain any space
+// 4. should be meaningful
+// 5. should be camel-cased
 
 
-let $_myFavouriteFlowerName = 'tulip' // correct naming
-const _ = 'something' // correct naming
+let $_myFavouriteFlowerName = 'tulip'; // correct naming
+const _something = 'something'; // correct naming
 
 // let wrong-VariableName = 'wrong naming'
+// let 2ndWorngVariableName = 'wrong'
 // This variable name will raise an error, 
 // because it is using a - (hyphen) which is a special character other than a _ or a $
 
-// ** copy a value of a variable to another variable
+
+// copy a value of a variable to another variable
+let numberOfStudentInClassTwo = numberOfStudentInClassOne;
+
+// console.log(numberOfStudentInClassTwo);
 
 // we can see output using console.log()
 // which is a built-in function from JS
-console.log(_)
+// console.log(_)
 
 
 
-console.log(myName)
-console.log(myAge)
+
 
 
--- lessons/session-02.js
+++ lessons/session-02.js
@@ -1,22 +1,67 @@
-// string, 
-// number, 
+// remaining to discuss:  object
 
-// boolean, null, undefined, object, symbol, BigInt
+// ********* expression **********//
+// for a programming language, an expression is anything that has a valid value
 
 
-let myName = 'omera'
-myName = 'akaram'
+// ********* boolean **********//
+// 1. boolean has two values - true, false
+// 2. true means yes, false means no
+// 3. we can manually assign a boolean value, or JS can compute it from another expression
 
-const myAge = 20
-
-let $_myMotherName = 'xyz'
-const _ = 'something'
-
-console.log(_)
+const omeraIsWebDev = true; // manually assigned
+const oneIsGreaterThanTwo = 1 > 2; // computed by JS - will learn more about it later
 
 
 
-console.log(myName)
-console.log(myAge)
+
+// ********* null **********//
+// It’s just a special value which represents “nothing”, “empty” or “value unknown”
+let theAgeOfThisLion = null;
 
 
+
+
+// ********* undefined **********//
+// 1. The meaning of undefined is “value is not assigned”
+// 2. can be manuallly assigned, or automatically computed to 'undefined' if you don't assign a value to a variable when you crate it
+let someNumber; // automatic
+let someOtherNumber = undefined; // manual
+
+someNumber = 'some value';
+
+
+
+// ********* typeof operator **********//
+// can be written in 2 ways 
+// 1. typeof expresseion
+// 2. typeof(expression)
+
+let typeOfMyName = typeof 'omera';
+let typeOfMyAge = typeof 20;
+
+
+
+
+// ********* Math Operators ********//
+// + - * / 
+
+// % - remainder operator
+// ** exponentiation operator 
+
+const one = 1;
+const two = 2;
+
+const addition = one + two;
+const substraction = one - two;
+const multi = one * two;
+const division = one / two;
+
+
+const value1 = 4;
+const value2 = 2;
+
+const remainder = value1 % value2;
+const exponentiation = value1 ** value2; // 4 ** 2 -> 4 ^ 2 -> 16
+
+console.log(remainder);
